9a9500af7036ca0906ed4398397935cd76da4cd6
Finn Gallagher-Mundy <P1414053X@LEC-ADMIN.dmu.ac.uk>
2017-12-12 15:40:47 +0000

    Hidden info panes show pointer when hovered over

---
+++ scripts/scripts.js
@@ -0,0 +1,7 @@
+(function(){
+	let infoPane1 = document.getElementById("info1");
+	let infoPane2 = document.getElementById("info2");
+	infoPane1.style.cursor = "pointer";
+	infoPane2.style.cursor = "pointer";
+	
+}());
